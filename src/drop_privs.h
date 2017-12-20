e2aa5f411f4cdb1a4e981c0eee61cb0c9e5f27f3
Dan Molik <dan@danmolik.com>
2017-12-20 09:56:12 -0500

    Project cleaning
      - linux syntax
      - subdir building

--- src/drop_privs.h
+++ src/drop_privs.h
@@ -43,21 +43,22 @@
 #include <security/pam_modules.h>
 #endif
 
-#define SAVED_GROUPS_MAX_LEN 64 /* as pam_modutil.. */
+#define SAVED_GROUPS_MAX_LEN 64	/* as pam_modutil.. */
 
 struct _ykpam_privs {
-  uid_t saved_euid;
-  gid_t saved_egid;
-  gid_t *saved_groups;
-  int saved_groups_length;
-  FILE *debug_file;
+	uid_t saved_euid;
+	gid_t saved_egid;
+	gid_t *saved_groups;
+	int saved_groups_length;
+	FILE *debug_file;
 };
 
 #define PAM_MODUTIL_DEF_PRIVS(n) \
   gid_t n##_saved_groups[SAVED_GROUPS_MAX_LEN]; \
   struct _ykpam_privs n = {-1, -1, n##_saved_groups, SAVED_GROUPS_MAX_LEN, cfg->debug_file}
 
-int pam_modutil_drop_priv(pam_handle_t *, struct _ykpam_privs *, struct passwd *);
+int pam_modutil_drop_priv(pam_handle_t *, struct _ykpam_privs *,
+			  struct passwd *);
 int pam_modutil_regain_priv(pam_handle_t *, struct _ykpam_privs *);
 
 #endif
--- src/util.h
+++ src/util.h
@@ -45,13 +45,15 @@
 } while (0)
 
 /* Return values for authorize_user_token and authorize_user_token_ldap */
-#define AUTH_NO_TOKENS -2 /* The user has no associated tokens */
-#define AUTH_ERROR      0 /* Internal error when looking up associated tokens */
-#define AUTH_FOUND      1 /* The requested token is associated to the user */
-#define AUTH_NOT_FOUND -1 /* The requested token is not associated to the user */
+#define AUTH_NO_TOKENS -2	/* The user has no associated tokens */
+#define AUTH_ERROR      0	/* Internal error when looking up associated tokens */
+#define AUTH_FOUND      1	/* The requested token is associated to the user */
+#define AUTH_NOT_FOUND -1	/* The requested token is not associated to the user */
 
-int get_user_cfgfile_path(const char *common_path, const char *filename, const struct passwd *user, char **fn);
-int check_user_token(const char *authfile, const char *username, const char *otp_id, int verbose, FILE *debug_file);
+int get_user_cfgfile_path(const char *common_path, const char *filename,
+			  const struct passwd *user, char **fn);
+int check_user_token(const char *authfile, const char *username,
+		     const char *otp_id, int verbose, FILE * debug_file);
 
 #if HAVE_CR
 #include <ykcore.h>
@@ -66,36 +68,40 @@ int check_user_token(const char *authfile, const char *username, const char *otp
 #define CR_DEFAULT_ITERATIONS 10000
 
 struct chalresp_state {
-  char challenge[CR_CHALLENGE_SIZE];
-  uint8_t challenge_len;
-  char response[CR_RESPONSE_SIZE];
-  uint8_t response_len;
-  char salt[CR_SALT_SIZE];
-  uint8_t salt_len;
-  uint8_t slot;
-  uint32_t iterations;
+	char challenge[CR_CHALLENGE_SIZE];
+	uint8_t challenge_len;
+	char response[CR_RESPONSE_SIZE];
+	uint8_t response_len;
+	char salt[CR_SALT_SIZE];
+	uint8_t salt_len;
+	uint8_t slot;
+	uint32_t iterations;
 };
 
 typedef struct chalresp_state CR_STATE;
 
 int generate_random(void *buf, int len);
 
-int check_user_challenge_file(const char *chalresp_path, const struct passwd *user, FILE *debug_file);
-int get_user_challenge_file(YK_KEY *yk, const char *chalresp_path, const struct passwd *user, char **fn, FILE *debug_file);
+int check_user_challenge_file(const char *chalresp_path,
+			      const struct passwd *user, FILE * debug_file);
+int get_user_challenge_file(YK_KEY * yk, const char *chalresp_path,
+			    const struct passwd *user, char **fn,
+			    FILE * debug_file);
 
-int load_chalresp_state(FILE *f, CR_STATE *state, bool verbose, FILE *debug_file);
-int write_chalresp_state(FILE *f, CR_STATE *state);
+int load_chalresp_state(FILE * f, CR_STATE * state, bool verbose,
+			FILE * debug_file);
+int write_chalresp_state(FILE * f, CR_STATE * state);
 
-int init_yubikey(YK_KEY **yk);
-int check_firmware_version(YK_KEY *yk, bool verbose, bool quiet, FILE *debug_file);
-int challenge_response(YK_KEY *yk, int slot,
-		       char *challenge, unsigned int len,
-		       bool hmac, bool may_block, bool verbose,
-		       char *response, unsigned int res_size, unsigned int *res_len);
+int init_yubikey(YK_KEY ** yk);
+int check_firmware_version(YK_KEY * yk, bool verbose, bool quiet,
+			   FILE * debug_file);
+int challenge_response(YK_KEY * yk, int slot, char *challenge, unsigned int len,
+		       bool hmac, bool may_block, bool verbose, char *response,
+		       unsigned int res_size, unsigned int *res_len);
 
-#endif /* HAVE_CR */
+#endif				/* HAVE_CR */
 
 size_t filter_result_len(const char *filter, const char *user, char *output);
 char *filter_printf(const char *filter, const char *user);
 
-#endif /* __PAM_YUBICO_UTIL_H_INCLUDED__ */
+#endif				/* __PAM_YUBICO_UTIL_H_INCLUDED__ */
